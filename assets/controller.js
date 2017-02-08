fd3fe97538dec8d492004b46f4367dd36c737817
chenshaobo <chenshaobo65@gmail.com>
2017-02-08 20:58:25 +0800

    upd

--- assets/controller.js
+++ assets/controller.js
@@ -16,12 +16,11 @@ function Action(val){
             id : 100004100,
             value : val
         }],
-        vibrate : 1, // 1为发送时振动，可选
-        nfc : 1, // 1为近场通信方式，可选
+       // vibrate : 1, // 1为发送时振动，可选
+       // nfc : 1, // 1为近场通信方式，可选
         lifetime : 20, // 生命期，范围1到604800秒，可选
         onSuccess : function (ret) { // 发送成功回调
             device.log('onSuccess: ' + JSON.stringify(ret));
-            console.log('onSuccess: ' +JSON.stringify(ret));
         },
         onAck : function (ret) { // 接收响应成功回调，蓝牙设备暂不支持
             device.log('onAck: ' + JSON.stringify(ret));
